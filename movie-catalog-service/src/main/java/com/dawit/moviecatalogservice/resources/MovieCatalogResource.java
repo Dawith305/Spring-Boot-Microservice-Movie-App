32b4994a878228c74c1955a44bc76e269030ed2b
Dawit <dawit@Dawits-MacBook-Pro.local>
2021-10-13 12:47:12 +0300

    Completed inter service communication without service discovery hardcoded urls

--- movie-catalog-service/src/main/java/com/dawit/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/dawit/moviecatalogservice/resources/MovieCatalogResource.java
@@ -4,12 +4,14 @@ package com.dawit.moviecatalogservice.resources;
 import com.dawit.moviecatalogservice.models.CatalogItem;
 import com.dawit.moviecatalogservice.models.Movie;
 import com.dawit.moviecatalogservice.models.Rating;
+import com.dawit.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -20,25 +22,41 @@ public class MovieCatalogResource {
 
     //injecting the defined bean singleton in this controller
     @Autowired()
-    RestTemplate restTemplate;
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
 
         //get the rating of all movies from ratingsdata service
-        List<Rating> ratings = Arrays.asList(
-                new Rating(1, 5),
-                new Rating(2, 7)
-        );
+//        List<Rating> ratings = Arrays.asList(
+//                new Rating(1, 5),
+//                new Rating(2, 7)
+//        );
+
+        UserRating ratings = restTemplate.getForObject("http://localhost:7073/ratingsdata/users/"+userId, UserRating.class);
 
         //stream consists of useful methods
         //for each movie rated get the movie info
         //put the result from both the service and return rated movies
-        return  ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:7072/movies/"+ rating.getId(), Movie.class);
+        return  ratings.getUserRating().stream().map(rating -> {
 
+            //using RestTemplate
+            Movie movie = restTemplate.getForObject("http://localhost:7072/movies/"+ rating.getId(), Movie.class);
             return  new CatalogItem(movie.getName(), "Sci-fi", rating.getRating());
+
+
+            //using Reactive Java Webclient Builder
+//            Movie movie = webClientBuilder.build()
+//                    .get()
+//                    .uri("http://localhost:7072/movies/" + rating.getId())
+//                    .retrieve()
+//                    .bodyToMono(Movie.class)
+//                    //block till the async mono returns the result
+//                    .block();
 
         }).collect(Collectors.toList());
 
--- rating-data-service/src/main/java/com/dawit/ratingdataservice/resource/RatingDataResource.java
+++ rating-data-service/src/main/java/com/dawit/ratingdataservice/resource/RatingDataResource.java
@@ -1,9 +1,14 @@
 package com.dawit.ratingdataservice.resource;
 
 import com.dawit.ratingdataservice.models.Rating;
+import com.dawit.ratingdataservice.models.UserRating;
+import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,5 +17,18 @@ public class RatingDataResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") int movieId){
         return new Rating(movieId,6);
+    }
+
+    @RequestMapping("/users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") int userId){
+        List<Rating> ratings  = Arrays.asList(
+                        new Rating(1, 5),
+                        new Rating(2, 7)
+        );
+
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+
+        return userRating;
     }
 }
